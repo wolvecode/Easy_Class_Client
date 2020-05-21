ce6d7aa9443c770dd687cee263c5ac4f14526701
wolvecode <wolvecode@gmail.com>
2020-05-21 23:44:12 +0100

    add submit page

---
+++ component/Submit.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default class SubmitPage extends React.Component {
+  render() {
+    return (
+      <div className="">
+        <h1 className="text-center pt-5 text-danger">This is add book pag</h1>
+      </div>
+    )
+  }
+}
